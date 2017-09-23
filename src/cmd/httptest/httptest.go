0ff0403744af7f43bef1d278fcb230a9c4b01985
luoyancn <luoyan_cn@outlook.com>
2017-09-23 18:39:27 +0800

    Add http example of golang

---
+++ src/cmd/httptest/httptest.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"httpgo"
+)
+
+func main() {
+	httpgo.RunServer()
+}
---
+++ src/httpgo/httpgo.go
@@ -0,0 +1,78 @@
+package httpgo
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type reqbody struct {
+	Name    string `json:"name"`
+	Id      int    `json:"id"`
+	Enabled bool   `json:"enabled,omitempty"`
+}
+
+func HttpGoHandler(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case http.MethodGet, http.MethodHead:
+		go_get_handler(writer, request)
+	case http.MethodPost:
+		go_post_handler(writer, request)
+	case http.MethodPut:
+		go_put_handler(writer, request)
+	case http.MethodDelete:
+		go_delete_handler(writer, request)
+	default:
+		go_unsupported_handler(writer, request)
+	}
+}
+
+func go_get_handler(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if nil != err {
+		http.Error(writer, "Cannot parse the http request\n",
+			http.StatusBadRequest)
+	}
+	poem_name := request.Form["name"]
+	if 0 == len(poem_name) {
+		fmt.Fprint(writer, "Visit the poem list\n")
+		return
+	}
+	fmt.Fprintf(writer, "Poem %s coming soon\n", poem_name[0])
+}
+
+func go_post_handler(writer http.ResponseWriter, request *http.Request) {
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		http.Error(writer, "Only json-liked request body is accepted\n",
+			http.StatusBadRequest)
+		return
+	}
+	var req_body reqbody
+	err = json.Unmarshal([]byte(body), &req_body)
+	if err != nil || "" == req_body.Name || 0 == req_body.Id {
+		http.Error(writer, "Failed to parse the request body in request\n",
+			http.StatusBadRequest)
+		return
+	}
+	writer.WriteHeader(http.StatusAccepted)
+	fmt.Fprintf(writer, "Recevied request body %v\n", string(body))
+}
+
+func go_put_handler(writer http.ResponseWriter, request *http.Request) {
+}
+
+func go_delete_handler(writer http.ResponseWriter, request *http.Request) {
+}
+
+func go_unsupported_handler(writer http.ResponseWriter,
+	request *http.Request) {
+	http.Error(writer, "Not supported http request method\n",
+		http.StatusMethodNotAllowed)
+}
+
+func RunServer() {
+	http.HandleFunc("/poem", HttpGoHandler)
+	http.ListenAndServe(":8080", nil)
+}
