7455a0cc315e4a15b9624fcf405c8bb160876a85
luoyancn <luoyan_cn@outlook.com>
2017-10-23 10:14:01 +0800

    Modify the unit tests for restgo

--- src/restgo/exceptions/exception_test.go
+++ src/restgo/exceptions/exception_test.go
@@ -11,3 +11,11 @@ func TestNewNotFoundException(t *testing.T) {
 		t.Error("Not expected error messages")
 	}
 }
+
+func TestNewNotFoundException_invalid(t *testing.T) {
+	fake_exception := NewNotFoundException("fake_resource_type", "fake_id")
+	if fake_exception.Error() ==
+		`fake_id with fake_resource_type cannot be found` {
+		t.Error("Not expected error messages")
+	}
+}
