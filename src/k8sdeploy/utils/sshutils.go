f674a22c180775d0f91b7c87a4ebf89714a824ef
luoyancn <luoyan_cn@outlook.com>
2017-11-17 20:02:19 +0800

    为k8sdeploy添加scp操作

--- src/k8sdeploy/utils/sshutils.go
+++ src/k8sdeploy/utils/sshutils.go
@@ -4,11 +4,13 @@ import (
 	"io/ioutil"
 	"k8sdeploy/logging"
 	"os"
+	"path"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
 
+	"github.com/pkg/sftp"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/ssh"
 )
@@ -40,34 +42,14 @@ func generate_ssh_auth_config() *ssh.ClientConfig {
 	return ssh_config_with_key
 }
 
-func SSHCheck(k8snodes ...string) bool {
-	logging.INFO.Printf("Waiting for ssh check for nodes :%v\n", k8snodes)
-	ssh_key := generate_ssh_auth_config()
-
-	check := make(chan bool, len(k8snodes))
-	runtime.GOMAXPROCS(len(k8snodes))
-	for _, node := range k8snodes {
-		go func(node string) {
-			logging.INFO.Printf("Connecting to the host %s ...\n", node)
-			conn, err := ssh.Dial(
-				"tcp", strings.Join([]string{node, "22"}, ":"), ssh_key)
-			if nil != err {
-				logging.ERROR.Printf(
-					"Cannot connect to the host %s: %v\n", node, err)
-				check <- false
-			}
-			defer conn.Close()
-			logging.INFO.Printf("Success connect to the host %s \n", node)
-			check <- true
-		}(node)
+func get_ssh_connection(host string,
+	ssh_key *ssh.ClientConfig) (*ssh.Client, error) {
+	conn, err := ssh.Dial(
+		"tcp", strings.Join([]string{host, "22"}, ":"), ssh_key)
+	if nil != err {
+		return nil, err
 	}
-
-	for index := 0; index < len(k8snodes); index++ {
-		if !<-check {
-			return false
-		}
-	}
-	return true
+	return conn, nil
 }
 
 func get_binary_files(path string) []string {
@@ -89,6 +71,109 @@ func get_binary_files(path string) []string {
 	return files
 }
 
+func scp_source_to_dest(ssh_conn *ssh.Client, binarys ...string) bool {
+	sftp_client, err := sftp.NewClient(ssh_conn)
+	if nil != err {
+		logging.ERROR.Printf("Cannot create scp tunnel:%v\n", err)
+		return false
+	}
+	defer sftp_client.Close()
+	dest_binary_path := viper.GetString("default.target_path")
+	_, err = sftp_client.Stat(dest_binary_path)
+	if nil != err {
+		logging.WARNING.Printf(
+			"The target binary path %s not exsit:%v, Try to create it ...\n",
+			dest_binary_path, err)
+		if err = sftp_client.Mkdir(dest_binary_path); nil != err {
+			logging.TRACE.Printf(
+				"Cannot create the path :%v\n", err)
+			return false
+		}
+	}
+
+	//	scp_res := make(chan bool, len(binarys))
+	//	runtime.GOMAXPROCS(len(binarys))
+	for _, binary := range binarys {
+		//		go func(binary string) {
+		logging.INFO.Printf("Scp file %s to remote ...\n", binary)
+		src_binary, err := os.Open(binary)
+		if nil != err {
+			logging.ERROR.Printf(
+				"Cannot read binary file %s:%v\n", binary, err)
+			//				scp_res <- false
+			//				return
+			return false
+		}
+		defer src_binary.Close()
+
+		_, dest_binary_name := filepath.Split(binary)
+		dest_binary, err := sftp_client.Create(
+			path.Join(dest_binary_path, dest_binary_name))
+		if err != nil {
+			logging.ERROR.Printf(
+				"Fail to create remote file %s in dest path %s :%v\n",
+				dest_binary_name, dest_binary_path, err)
+			//				scp_res <- false
+			//				return
+			return false
+		}
+		defer dest_binary.Close()
+
+		buf := make([]byte, 1024)
+		for {
+			n, _ := src_binary.Read(buf)
+			if n == 0 {
+				break
+			}
+			dest_binary.Write(buf[0:n])
+		}
+		//			scp_res <- true
+		sftp_client.Chmod(
+			path.Join(dest_binary_path, dest_binary_name), 0755)
+		logging.INFO.Printf("Success scp file %s to remote\n", binary)
+		//		}(binary)
+	}
+
+	/*
+		for index := 0; index < len(binarys); index++ {
+			if !<-scp_res {
+				return false
+			}
+		}
+	*/
+	return true
+}
+
+func SSHCheck(k8snodes ...string) bool {
+	logging.INFO.Printf("Waiting for ssh check for nodes :%v\n", k8snodes)
+	ssh_key := generate_ssh_auth_config()
+
+	check := make(chan bool, len(k8snodes))
+	runtime.GOMAXPROCS(len(k8snodes))
+	for _, node := range k8snodes {
+		go func(node string) {
+			logging.INFO.Printf("Connecting to the host %s ...\n", node)
+			conn, err := get_ssh_connection(node, ssh_key)
+			if nil != err {
+				logging.ERROR.Printf(
+					"Cannot connect to the host %s: %v\n", node, err)
+				check <- false
+				return
+			}
+			check <- true
+			defer conn.Close()
+			logging.INFO.Printf("Success connect to the host %s\n", node)
+		}(node)
+	}
+
+	for index := 0; index < len(k8snodes); index++ {
+		if !<-check {
+			return false
+		}
+	}
+	return true
+}
+
 func SCPBinary(binary_type string, k8snodes ...string) bool {
 	binary_files_path := viper.GetString(
 		strings.Join([]string{binary_type, "binary_path"}, "."))
@@ -101,5 +186,40 @@ func SCPBinary(binary_type string, k8snodes ...string) bool {
 	}
 	logging.DEBUG.Printf("The %s binary files were follows: %v\n",
 		binary_type, files)
+
+	ssh_res := make(chan bool, len(k8snodes))
+	runtime.GOMAXPROCS(len(k8snodes))
+
+	for _, node := range k8snodes {
+		go func(node string) {
+			logging.INFO.Printf("Scp %s binarys to host %s ...\n",
+				binary_type, node)
+			ssh_conn, err := get_ssh_connection(
+				node, generate_ssh_auth_config())
+			if nil != err {
+				logging.ERROR.Printf(
+					"Cannot scp the %s binarys to %s: %v\n",
+					binary_type, node, err)
+				ssh_res <- false
+				return
+			}
+			defer ssh_conn.Close()
+			if !scp_source_to_dest(ssh_conn, files...) {
+				logging.ERROR.Printf(
+					"Fail to scp the %s binarys to %s\n", binary_type, node)
+				ssh_res <- false
+				return
+			}
+			logging.INFO.Printf(
+				"Scp %s binarys to host %s success \n", binary_type, node)
+			ssh_res <- true
+		}(node)
+	}
+
+	for index := 0; index < len(k8snodes); index++ {
+		if !<-ssh_res {
+			return false
+		}
+	}
 	return true
 }
