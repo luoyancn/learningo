0552382992ed8b19dd1862386176bd55e98f2555
luoyan_cn <luoyan_cn@outlook.com>
2017-11-25 16:49:11 +0800

    Ensure init logger only once in projects

--- src/k8sdeploy/logging/logging.go
+++ src/k8sdeploy/logging/logging.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"log"
 	"os"
+	"sync"
 
 	gologging "github.com/op/go-logging"
 	"github.com/spf13/viper"
@@ -21,25 +22,29 @@ var format_file = gologging.MustStringFormatter(
 		" %{shortfile} %{shortfunc} %{message}",
 )
 
+var once sync.Once
+
 func GetLogger() {
-	logfile, err := os.OpenFile(
-		viper.GetString("default.log_file"),
-		os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("%v\n", err)
-	}
-	file_backend := gologging.NewLogBackend(logfile, "", 0)
-	std_backend := gologging.NewLogBackend(os.Stdout, "", 0)
+	once.Do(func() {
+		logfile, err := os.OpenFile(
+			viper.GetString("default.log_file"),
+			os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		file_backend := gologging.NewLogBackend(logfile, "", 0)
+		std_backend := gologging.NewLogBackend(os.Stdout, "", 0)
 
-	file_back_formater := gologging.NewBackendFormatter(
-		file_backend, format_file)
-	std_back_formater := gologging.NewBackendFormatter(
-		std_backend, format_std)
+		file_back_formater := gologging.NewBackendFormatter(
+			file_backend, format_file)
+		std_back_formater := gologging.NewBackendFormatter(
+			std_backend, format_std)
 
-	gologging.SetBackend(file_back_formater, std_back_formater)
-	if !viper.GetBool("default.debug") {
-		gologging.SetLevel(gologging.DEBUG, "")
-	} else {
-		gologging.SetLevel(gologging.INFO, "")
-	}
+		gologging.SetBackend(file_back_formater, std_back_formater)
+		if !viper.GetBool("default.debug") {
+			gologging.SetLevel(gologging.DEBUG, "")
+		} else {
+			gologging.SetLevel(gologging.INFO, "")
+		}
+	})
 }
